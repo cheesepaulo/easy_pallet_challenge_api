2b80776790a1563c61d01f1e2dc7a0ebfefc0ab7
Paulo Cesar <pcdsjr@gmail.com>
2020-03-14 08:33:26 -0500

    test loads return an serilalized json

--- spec/requests/api/v1/loads_spec.rb
+++ spec/requests/api/v1/loads_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Api::V1::Loads", type: :request do
   describe "GET /api/v1/loads" do
 
     before do
-      create_list(:load, 10)
+      @loads = create_list(:load, 10)
       get '/api/v1/loads'
     end
 
@@ -12,6 +12,10 @@ RSpec.describe "Api::V1::Loads", type: :request do
 
     it 'returns a list of elemments' do
       expect(json.count).to eql(10)
+    end
+
+    it 'returns a serialized list' do
+      expect(json).to eql(each_serialized(Api::V1::LoadSerializer, @loads))
     end
   end
 end
