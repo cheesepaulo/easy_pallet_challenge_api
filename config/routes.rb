9318569ccda730b67a6d125f0341956d94f4dc28
Paulo Cesar <pcdsjr@gmail.com>
2020-03-13 21:30:33 -0500

    add order routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :loads, only: [:index, :show]
+      resources :orders, only: [:show]
     end
   end
 end
