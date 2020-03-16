b19e6e13614b078470df26c3fd75eb24d45237d7
Paulo Cesar <pcdsjr@gmail.com>
2020-03-16 07:20:39 -0500

    create request test for POST api/v1/:id/organize

--- spec/requests/api/v1/orders_spec.rb
+++ spec/requests/api/v1/orders_spec.rb
@@ -33,4 +33,31 @@ RSpec.describe "Api::V1::Orders", type: :request do
       it { expect(response).to have_http_status(:not_found) }
     end
   end
+
+  describe "POST /api/v1/order/:id/organize" do
+
+    let(:order) { create(:order) }
+
+    context 'when order has a list of order_products' do
+      before do
+        create_list(:order_product, 5, order: order)
+        post "/api/v1/order/#{order.id}/organize"
+      end
+
+      it { expect(response).to have_http_status(:created) }
+    end
+
+    context 'when order are empty' do
+      before do
+        create_list(:order_product, 5)
+        post "/api/v1/order/#{order.id}/organize"
+      end
+
+      it { expect(response).to have_http_status(:no_content) }
+
+      it 'not create a list of ordenated_order_products' do
+        expect(Order.find(order.id).ordenated_order_products.present?).to be false
+      end
+    end
+  end
 end
