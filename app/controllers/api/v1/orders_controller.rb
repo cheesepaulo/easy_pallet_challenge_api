26f3f34a7265c8b710c92e9aafa81d473cd31b46
Paulo Cesar <pcdsjr@gmail.com>
2020-03-16 11:02:00 -0500

    refactore orders controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -6,15 +6,12 @@ class Api::V1::OrdersController < Api::V1::BaseController
   end
 
   def organize
-    if @order.order_products.present?
-      result = Api::V1::OrganizeOrderService.new(@order).call
-      if result == true
+    if order_can_be_organized?
+      if Api::V1::OrganizeOrderService.new(@order).call == true
         render json: "Order organized successful.", status: :created
       else
-        render json: "This request could not be organized", status: :no_content
+        render json: "This request could not be organized.", status: :bad_request
       end
-    else
-      render json: "It is not possible to organize an empty order", status: :no_content
     end
   end
 
@@ -22,5 +19,17 @@ class Api::V1::OrdersController < Api::V1::BaseController
 
   def set_order
     @order = Order.find(params[:id])
+  end
+
+  def order_can_be_organized?
+    if @order.ordenated_order_products.present?
+      render json: "It is not possible to organize an order already organized.", status: :bad_request
+      return false
+    elsif @order.order_products.present?
+      return true
+    else
+      render json: "It is not possible to organize an empty order.", status: :bad_request
+      return false
+    end
   end
 end
