ccf1b8f57cbaa86dc24b55536c95ef952dd37272
Paulo Cesar <pcdsjr@gmail.com>
2020-03-16 23:26:53 -0500

    include a boolean field organized? at order_serializer

--- app/serializers/api/v1/order_serializer.rb
+++ app/serializers/api/v1/order_serializer.rb
@@ -1,4 +1,8 @@
 class Api::V1::OrderSerializer < ActiveModel::Serializer
-  attributes :id, :load_id, :code, :bay
+  attributes :id, :load_id, :code, :bay, :organized
   has_many :order_products, serializer: Api::V1::OrderProductSerializer
+
+  def organized
+    object.ordenated_order_products.present?
+  end
 end
