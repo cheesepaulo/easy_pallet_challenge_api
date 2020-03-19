f4cc520b5a26f07551c46a5f491502eebba60b47
Paulo Cesar <pcdsjr@gmail.com>
2020-03-18 20:19:32 -0500

    change deploy folders

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,17 +3,16 @@ lock "~> 3.12.1" # config valid for current version and patch releases of Capist
 server '144.202.21.153', port: 22, roles: [:app, :db], primary: true
 
 set :repo_url,        'git@github.com:cheesepaulo/easy_pallet_challenge_api.git'
-set :application,     'easy-pallete-challenge-api'
+set :application,     'api'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
-set :puma_workers,    2
+set :puma_workers,    0
 
-# Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/easy_pallet_challenge/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
@@ -25,7 +24,7 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 
 ## Defaults:
-set :branch, "master" # Branch alvo para o deploy
+set :branch, "master"
 set :log_level, :debug
 set :keep_releases, 5
 set :bundle_flags, '--deployment'
@@ -62,7 +61,8 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    invoke 'puma:smart_restart'
+    invoke 'puma:stop'
+    invoke 'puma:start'
   end
 
   before :starting,     :check_revision
