3962211f85d48a999a6f64e275f0a362776d5354
Paulo Cesar <pcdsjr@gmail.com>
2020-03-21 13:37:31 -0500

    include delivery_date formated at load detail serializer

--- app/serializers/api/v1/load_detail_serializer.rb
+++ app/serializers/api/v1/load_detail_serializer.rb
@@ -1,4 +1,8 @@
 class Api::V1::LoadDetailSerializer < ActiveModel::Serializer
   attributes :id, :code, :delivery_date
   has_many :orders
+
+  def delivery_date
+    self.object.delivery_date.strftime("%d/%m/%Y %H:%M")
+  end
 end
