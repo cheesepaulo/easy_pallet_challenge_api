5d7b7284f54fd3558c1ec4efb745e98cac6362ce
Paulo Cesar <pcdsjr@gmail.com>
2020-03-22 09:44:23 -0500

    remove unused serializer

--- app/serializers/api/v1/load_detail_serializer.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::LoadDetailSerializer < ActiveModel::Serializer
-  attributes :id, :code, :delivery_date
-  has_many :orders
-
-  def delivery_date
-    self.object.delivery_date.strftime("%d/%m/%Y %H:%M")
-  end
-end
