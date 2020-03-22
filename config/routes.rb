73e9aeb719596f57fe4bdfc43edc47af888e7a23
Paulo Cesar <pcdsjr@gmail.com>
2020-03-21 20:08:39 -0500

    add create action for order_products

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      
       resources :loads do
         resources :orders, only: [:create]
       end
-      resources :products
-      resources :orders, only: [:show, :update, :destroy]
+
+      resources :orders, only: [:show, :update, :destroy] do
+        resources :order_products, only: [:create]
+      end
+      
       post '/order/:id/organize', to: 'orders#organize'
       get '/orders/:id/ordenated', to: 'orders#show_ordenated'
+      
+      resources :products
     end
   end
 end
